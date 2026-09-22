7c421e20ca78dca7fd7d30ecf58a06ce96711162
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-09-22 19:58:06 +0300

    Update types.js

--- packages/vm/src/types.js
+++ packages/vm/src/types.js
@@ -1,5 +1,10 @@
 class DomElement {
-    constructor (value) {}
+    constructor (value) {
+        this.element = document.createElement(value.name)
+        for (let i = 0; i < Object.keys(value.args).lenght; i++) {
+            this.element.setAttribute(Object.keys(value.args)[i], value.args[Object.keys(value.args)[i]])
+        }
+    }
 }
 
 export {
