2fcdbaae65ebad1ca80ffcf1846217b2d5501c47
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-09-22 20:07:40 +0300

    dom.js: ⬆ Update

--- packages/vm/src/blocks/dom.js
+++ packages/vm/src/blocks/dom.js
@@ -6,11 +6,47 @@ class ExtensionDOM {
             "getBody": { blockType: "DomElement", function: this.getBody },
             "createElement": { blockType: "DomElement", function: this.createElement }
         }
+        this.reporters = {
+            "getBody": { blockType: "DomElement", function: this.getBody },
+            "createElement": { blockType: "DomElement", function: this.createElement }
+        }
     }
 
-    getBody () {}
+    getBody () {
+        return document.body
+    }
 
-    createElement () {}
+    createElement (args) {
+        return new DomElement(args.type).element
+    }
+
+    setAttribute (args) {
+        args.element.setAttribute(args.name, args.value)
+        return args.element
+    }
+
+    setTextContent (args) {
+        args.element.textContent = args.value
+        return args.element
+    }
+
+    setContent (args) {
+        args.element.innerHTML = args.value
+        return args.element
+    }
+
+    appendChild (args) {
+        args.element.appendChild(args.child)
+        return args.element
+    }
+
+    textContent (args) {
+        return args.element.textContent
+    }
+
+    content (args) {
+        return args.element.innerHTML
+    }
 }
 
 export default ExtensionDOM
